c72907d7ac1a01ced2a6297734f246cee7dba7b0
Boryslav <boryslavw@nycstudents.net>
2024-10-27 19:33:03 -0400

    added a sentence to fastest, and made the slowest

---
+++ src/Slowest.java
@@ -0,0 +1,7 @@
+public class Slowest {
+    public static void main(String[] args){
+
+        System.out.println("Im the slowest dude, probably in my town idk");
+
+    }
+}
--- src/trackRunner.java
+++ src/trackRunner.java
@@ -3,6 +3,7 @@ public class trackRunner {
 
         System.out.println("Im the fastest man in history");
         int topSpeed = (int) (Math.random() * 15) + 27;
+        System.out.println("hold on, lemme calculate it");
         System.out.println("I can run " + topSpeed + "mph");
 
         System.out.println("Nah, id win");
