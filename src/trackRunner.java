83852aff002b9423156832e9a3b2cb3129446ec3
Boryslav <boryslavw@nycstudents.net>
2024-10-27 19:20:11 -0400

    added some logic

--- src/trackRunner.java
+++ src/trackRunner.java
@@ -2,6 +2,8 @@ public class trackRunner {
     public static void main(String[] args) {
 
         System.out.println("Im the fastest man alive");
+        int speed = (int) (Math.random() * 15) + 25;
+        System.out.println("I can run " + speed + "mph");
 
     }
 }
